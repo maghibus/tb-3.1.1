b08d23d789ab5fa83274f198ad5ec24568f681f0
unknown <a.scagnoli@almaviva.it>
2020-10-19 16:50:46 +0200

    Authentication with token generated from oneadmin, user devuser/Almaviva1!

--- ui-ngx/proxy.conf.js
+++ ui-ngx/proxy.conf.js
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
-const forwardUrl = "http://demo.thingsboard.io";
+// const forwardUrl = "http://demo.thingsboard.io";
+// const wsForwardUrl = "ws://demo.thingsboard.io";
+const forwardUrl = "http://localhost:8080";
 const wsForwardUrl = "ws://demo.thingsboard.io";
 const ruleNodeUiforwardUrl = forwardUrl;
 
