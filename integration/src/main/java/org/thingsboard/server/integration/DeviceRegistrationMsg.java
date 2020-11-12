30e9de7de92357a24fb1e39e316e35bef6eb9626
unknown <a.scagnoli@almaviva.it>
2020-11-12 12:31:13 +0100

    jsonignore properties added to DeviceRegistrationMsg

--- integration/src/main/java/org/thingsboard/server/integration/DeviceRegistrationMsg.java
+++ integration/src/main/java/org/thingsboard/server/integration/DeviceRegistrationMsg.java
@@ -15,6 +15,7 @@
  */
 package org.thingsboard.server.integration;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import org.thingsboard.server.common.data.id.CustomerId;
