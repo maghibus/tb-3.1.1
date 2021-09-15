fe74e3a9ac654d05d97816905b02a6505057dda0
a.cosenza <a.cosenza@almaviva.it>
2021-09-15 10:44:37 +0200

    import exception

--- application/src/main/java/org/thingsboard/server/service/install/migrate/CassandraToSqlTable.java
+++ application/src/main/java/org/thingsboard/server/service/install/migrate/CassandraToSqlTable.java
@@ -34,6 +34,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Data
