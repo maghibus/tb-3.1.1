5000b230b82131378b34fc0d40dde4598ed31f7a
Federico Serini <F.Serini@almaviva.it>
2022-05-19 14:36:19 +0200

    add: kafka ssl rule chain block

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/kafka/custom/KafkaProperties.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/kafka/custom/KafkaProperties.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2016-2020 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.rule.engine.kafka.custom;
 
 import lombok.experimental.UtilityClass;
@@ -31,6 +46,10 @@ public class KafkaProperties {
         kafkaProperties.put(SSL_ENGINE_FACTORY_CLASS, CustomSslEngineFactory.class);
         kafkaProperties.put(SSL_KEYSTORE_BYTEARRAY, Base64.getDecoder().decode(configuration.getKeystore()));
         kafkaProperties.put(SSL_TRUSTSTORE_BYTEARRAY, Base64.getDecoder().decode(configuration.getTruststore()));
+        kafkaProperties.put("ssl.truststore.password", configuration.getPassword());
+        kafkaProperties.put("ssl.keystore.password", configuration.getPassword());
+        kafkaProperties.put("ssl.key.password", configuration.getPassword());
+
         return kafkaProperties;
     }
 
